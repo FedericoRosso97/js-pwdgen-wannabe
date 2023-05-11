e7e5c7aad98723bbe06d1790f57c370a6d28ee33
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-11 16:18:22 +0200

    stampo in pagina la password

---
+++ node.js
@@ -0,0 +1,10 @@
+const userName=prompt('nome utente');
+const userSurName=prompt('cognome utente');
+const favoriteColor=prompt('colore preferito');
+
+
+let password= userName+userSurName+favoriteColor+21;
+
+document.getElementById('form').innerHTML=password;
+
+console.log(password);
