4d3c1596c5e4f7e84d4252f206cd7b931b0be56e
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-11 16:54:14 +0200

    rendo 21 alla fine della password una stringa

--- node.js
+++ node.js
@@ -2,8 +2,7 @@ const userName=prompt('nome utente');
 const userSurName=prompt('cognome utente');
 const favoriteColor=prompt('colore preferito');
 
-
-const password= userName+userSurName+favoriteColor+21;
+const password= userName+userSurName+favoriteColor+"21";
 
 document.getElementById('form').innerHTML=password;
 
